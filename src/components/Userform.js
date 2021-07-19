b1c6376c2f23584f183cc39850adf8925008b42a
awsprepaid2@mml.local-at-867636236097 <awsprepaid2@mml.local-at-867636236097>
2021-07-19 12:42:46 +0530

    added age field

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -1,16 +1,28 @@
 import { useState } from "react"
 
 
-export default function Userform(){
-    const [userform, setUserform] = useState({firstname: "Ram"}); //hook -function
-    const handleEvent = function (event){
+export default function Userform() {
+    const [userform, setUserform] = useState({
+        firstname: "Ram",
+        age: 20
+    }); //hook -function
+    const handleEvent = function (event) {
         console.log(event);
-        setUserform({...userform, firstname:event.target.value})
+        setUserform({ ...userform, firstname: event.target.value })
+    }
+    const handleEventForAge = function (event) {
+        console.log(event);
+        setUserform({ ...userform, age: event.target.value })
+    }
+    const save = function (event) {
+        console.log(userform);
     }
     return (
         <div>
             <h3>Create User</h3>
             <input value={userform.firstname} onChange={handleEvent}></input>
+            <input value={userform.age} onChange={handleEventForAge}></input>
+            <button onClick={save}>Save</button>
         </div>
     )
 }

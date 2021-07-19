e80aa93f1ea761a53d092255e226cec848be816a
awsprepaid2@mml.local-at-867636236097 <awsprepaid2@mml.local-at-867636236097>
2021-07-19 14:20:08 +0530

    generic event handler

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -1,6 +1,5 @@
 import { useState } from "react"
 
-
 export default function Userform() {
     const [userform, setUserform] = useState({
         firstname: "Ram",
@@ -8,20 +7,17 @@ export default function Userform() {
     }); //hook -function
     const handleEvent = function (event) {
         console.log(event);
-        setUserform({ ...userform, firstname: event.target.value })
+        setUserform({ ...userform, [event.target.name]: event.target.value })
     }
-    const handleEventForAge = function (event) {
-        console.log(event);
-        setUserform({ ...userform, age: event.target.value })
-    }
+    
     const save = function (event) {
         console.log(userform);
     }
     return (
         <div>
             <h3>Create User</h3>
-            <input value={userform.firstname} onChange={handleEvent}></input>
-            <input value={userform.age} onChange={handleEventForAge}></input>
+            <input name='firstname' value={userform.firstname} onChange={handleEvent}></input>
+            <input name='age' value={userform.age} onChange={handleEvent}></input>
             <button onClick={save}>Save</button>
         </div>
     )

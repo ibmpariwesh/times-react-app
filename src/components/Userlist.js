9d47ea91f008fb31cd5dc16f66f1030f45ef87f0
awsprepaid2@mml.local-at-867636236097 <awsprepaid2@mml.local-at-867636236097>
2021-07-20 10:59:28 +0530

    using arrow functions

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -3,16 +3,13 @@ import { useEffect, useState } from "react"
 
 export default function Userlist() {
     const [users, setUsers] = useState([]);
-    useEffect(function () {
-        if(users.length !=0){
+    useEffect(() => {
+        if (users.length != 0) {
             return;
         }
         console.log('called......................');
         const promise = axios.get("http://localhost:4200/users");
-        promise.then(function (response) {
-            setUsers(response.data);
-            console.log(response.data);
-        })
+        promise.then(response => setUsers(response.data))
     })
     return (
         <div>
@@ -22,12 +19,10 @@ export default function Userlist() {
                         <th>Age</th></tr>
                 </thead>
                 <tbody>
-                    {users.map(function (user, index) {
-                        return <tr>
-                            <td>{user.firstname}</td>
-                            <td>{user.age}</td>
-                        </tr>
-                    })}
+                    {users.map((user, index) => <tr>
+                        <td>{user.firstname}</td>
+                        <td>{user.age}</td>
+                    </tr>)}
                 </tbody>
             </table>
         </div>

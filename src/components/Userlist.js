b31cf702b0b1505ff19b35f46bc1988d7909c4c0
awsprepaid2@mml.local-at-867636236097 <awsprepaid2@mml.local-at-867636236097>
2021-07-20 10:43:06 +0530

    display users in List

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -1,19 +1,33 @@
-import { useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 export default function Userlist() {
     const [users, setUsers] = useState([]);
+    useEffect(function () {
+        if(users.length !=0){
+            return;
+        }
+        console.log('called......................');
+        const promise = axios.get("http://localhost:4200/users");
+        promise.then(function (response) {
+            setUsers(response.data);
+            console.log(response.data);
+        })
+    })
     return (
         <div>
-            <table className=' table table-bordered table-hover table-responsive'>
+            <table className=' table table-bordered table-hover table-responsive table-striped'>
                 <thead>
-                    <tr><td>First name</td>
-                    <td>Age</td></tr>
+                    <tr><th>First name</th>
+                        <th>Age</th></tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Pariwesh</td>
-                        <td>34</td>
-                    </tr>
+                    {users.map(function (user, index) {
+                        return <tr>
+                            <td>{user.firstname}</td>
+                            <td>{user.age}</td>
+                        </tr>
+                    })}
                 </tbody>
             </table>
         </div>

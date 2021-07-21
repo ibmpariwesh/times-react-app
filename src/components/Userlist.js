92320aaec49a38d980ff28202f771d602acc7b5c
awsprepaid2@mml.local-at-867636236097 <awsprepaid2@mml.local-at-867636236097>
2021-07-21 10:59:39 +0530

    added delete user featuer

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -1,20 +1,30 @@
 import axios from "axios";
-import { Button } from "bootstrap";
 import { useEffect, useState } from "react"
 import Counter from "./Counter";
 
 export default function Userlist() {
     const [users, setUsers] = useState([]);
     useEffect(() => {
-        if (users.length !== 0) {
-            return;
-        }
+        // if (users.length !== -1) {
+        //     return;
+        // }
         console.log('called......................');
         const promise = axios.get("http://localhost:4200/users");
         promise.then(response => setUsers(response.data))
-    })
+    }, [])
+    const deleteUser = function () {
+        // confirm("are you sure");
+        const promise = axios.delete("http://localhost:4200/users/" + this);
+        promise.then(response => {
+            //remove delete record from list
+            users.splice(arguments[0], 1);
+            const users1 = [...users]; //time vs space complexity
+            setUsers(users1);
+        })
+    }
     return (
-        <div><Counter count={users.length}></Counter>
+        <div>
+            {/* <Counter count={users.length}></Counter> */}
             <table className=' table table-compact table-bordered table-hover table-responsive table-striped'>
                 <thead>
                     <tr><th>First name</th>
@@ -23,11 +33,11 @@ export default function Userlist() {
                         <th>??</th></tr>
                 </thead>
                 <tbody>
-                    {users.map((user, index) => <tr key={index}>
+                    {users.map((user, index) => <tr key={user.id}>
                         <td>{user.firstname}</td>
                         <td>{user.age}</td>
                         <td>{user.joiningDate}</td>
-                        <td><button className='btn btn-danger'>Delete</button></td>
+                        <td><button className='btn btn-danger' onClick={deleteUser.bind(user.id, index)}>Delete</button></td>
                     </tr>)}
                 </tbody>
             </table>

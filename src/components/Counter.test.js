690ad70d4519a19b0f8bfd51fb11e0a54d1b5f1d
awsprepaid2@mml.local-at-867636236097 <awsprepaid2@mml.local-at-867636236097>
2021-07-27 14:56:39 +0530

    added jest enzyme testing  file

---
+++ src/components/Counter.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Counter from "./Counter";
+
+test('scenario 1', ()=>{
+    render(<Counter  count='5'/>);
+  const element = screen.getByText("5");
+    console.log('testing....');
+    expect(element.innerHTML).toBe("5");
+    expect(element.tagName).toEqual('SPAN');
+})
